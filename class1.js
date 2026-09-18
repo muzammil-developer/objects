3dc6eed2378cc42fa2b0958424446d206151e05d
Muzammil Hussain <itz.muzz787@gmail.com>
2026-09-18 12:54:36 +0500

    Add student1 object with details and output text

---
+++ class1.js
@@ -0,0 +1,13 @@
+var student1 = {
+    name: 'Muzammil',
+    batch: 22,
+    course: 'web development',
+    timings: '11am - 1pm',
+    skills: ['web development', 'graphic designing']
+};
+
+console.log(student1)
+
+var text = `${student1.name} is student of batch ${student1.batch}. He is learning ${student1.course}`
+
+document.write(text)
